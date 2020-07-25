ae08144eaa73cfa1e923ea35faf197e360c06feb
zogpap <zogpap@163.com>
2020-07-25 22:40:50 +0800

    WKNativeTypeAction替换UIWebviewNaviType

--- ios/RCTWebViewBridgeManager.m
+++ ios/RCTWebViewBridgeManager.m
@@ -18,6 +18,9 @@
 
 #import <React/UIView+React.h>
 
+#import <WebKit/WebKit.h>
+
+
 @interface RCTWebViewBridgeManager () <RCTWebViewBridgeDelegate>
 
 @end
@@ -58,12 +61,12 @@ RCT_REMAP_VIEW_PROPERTY(keyboardDisplayRequiresUserAction, _webView.keyboardDisp
   return @{
     @"JSNavigationScheme": RCTJSNavigationScheme,
     @"NavigationType": @{
-      @"LinkClicked": @(UIWebViewNavigationTypeLinkClicked),
-      @"FormSubmitted": @(UIWebViewNavigationTypeFormSubmitted),
-      @"BackForward": @(UIWebViewNavigationTypeBackForward),
-      @"Reload": @(UIWebViewNavigationTypeReload),
-      @"FormResubmitted": @(UIWebViewNavigationTypeFormResubmitted),
-      @"Other": @(UIWebViewNavigationTypeOther)
+      @"LinkClicked": @(WKNavigationTypeLinkActivated),
+      @"FormSubmitted": @(WKNavigationTypeFormSubmitted),
+      @"BackForward": @(WKNavigationTypeBackForward),
+      @"Reload": @(WKNavigationTypeReload),
+      @"FormResubmitted": @(WKNavigationTypeFormResubmitted),
+      @"Other": @(WKNavigationTypeOther)
     },
   };
 }
